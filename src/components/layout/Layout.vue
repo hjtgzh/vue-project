676c0c5e07ee8c937fe4904abaa55bdc03f12c0b
hjtgzh1 <huangjianting@zhiweicloud.com>
2017-12-08 18:03:49 +0800

    router嵌套

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,35 +1,40 @@
 <template>
-    <div class="layout-wrap">
-        <el-menu class="el-menu-vertical-demo" @select="handleSelect" :collapse="isCollapse">
-            <el-submenu index="1">
-                <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                    <span slot="title">分组一</span>
-                    <el-menu-item index="table">列表</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                    <span slot="title">选项4</span>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+    <div class="content">
+        <div class="layout-wrap">
+            <el-menu class="el-menu-vertical-demo" @select="handleSelect" :collapse="isCollapse">
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span slot="title">导航一</span>
+                    </template>
+                    <el-menu-item-group>
+                        <span slot="title">分组一</span>
+                        <el-menu-item index="table">列表</el-menu-item>
+                        <el-menu-item index="1-2">选项2</el-menu-item>
+                    </el-menu-item-group>
+                        <el-menu-item-group title="分组2">
+                        <el-menu-item index="1-3">选项3</el-menu-item>
+                    </el-menu-item-group>
+                    <el-submenu index="1-4">
+                        <span slot="title">选项4</span>
+                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                    </el-submenu>
                 </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航三</span>
-            </el-menu-item>
-        </el-menu>
-        <div class="collapse-wrap" :class="[ isCollapse ? 'collapsed' : '']" @click="handleCollapse">
-            <i :class="[ isCollapse ? 'el-icon-d-arrow-left' : 'el-icon-d-arrow-right']"></i>
+                <el-menu-item index="2">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">导航三</span>
+                </el-menu-item>
+            </el-menu>
+            <div class="collapse-wrap" :class="[ isCollapse ? 'collapsed' : '']" @click="handleCollapse">
+                <i :class="[ isCollapse ? 'el-icon-d-arrow-left' : 'el-icon-d-arrow-right']"></i>
+            </div>
+        </div>
+        <div class="link-content">
+            <router-view></router-view>
         </div>
     </div>
 </template>
@@ -44,7 +49,7 @@
         methods: {
             handleSelect (key, keyPath) {
                 console.log(key, keyPath)
-                this.$router.push('/table')
+                this.$router.push('/layout/table')
             },
             handleCollapse () {
                 this.isCollapse = !this.isCollapse
@@ -74,5 +79,8 @@
         .collapsed{
             width: 64px;
         }
+    }
+    .link-content{
+        padding: 0 20px 0 220px;
     }
 </style>
